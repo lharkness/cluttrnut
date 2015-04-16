16a82a43100d927b5e31a9497d687edc8d85bd33
Lee <lee@leeharkness.com>
2015-04-15 19:50:14 -0500

    More pricing struggles

--- packages/system/public/controllers/pricing.js
+++ packages/system/public/controllers/pricing.js
@@ -17,20 +17,8 @@ exports.render = function(req, res) {
         });
     }
 
-    function isAdmin() {
-        return req.user && req.user.roles.indexOf('admin') !== -1;
-    }
-
     // Send some basic starting info to the view
     res.render('pricing', {
-        user: req.user ? {
-            name: req.user.name,
-            _id: req.user._id,
-            username: req.user.username,
-            roles: req.user.roles
-        } : {},
-        modules: modules,
-        isAdmin: isAdmin,
-        adminEnabled: isAdmin() && mean.moduleEnabled('mean-admin')
+        modules: modules
     });
 };
